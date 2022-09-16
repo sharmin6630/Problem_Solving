8ae8da68286d82c2474fdb137cd839fc4b654150
Sharmin Ahmed <sharminahmed2010@gmail.com>
2022-09-17 02:22:06 +0600

    two sum

---
+++ LeetCode/1. Two Sum/1. Two Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+}
